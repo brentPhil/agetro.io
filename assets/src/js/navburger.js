4ffe9f6d1c201bbe41dc856fee2fa9714a9ab0e5
brentPhil <82448930+brentPhil@users.noreply.github.com>
2021-06-14 16:12:51 +0800

    Add files via upload
    initial commit

---
+++ assets/src/js/navburger.js
@@ -0,0 +1,35 @@
+const menuBtn = 
+            document.querySelector(".menu-btn");
+  
+        const menu = 
+            document.querySelector(".menu");
+  
+        const menuNav = 
+            document.querySelector(".menu-nav");
+  
+        const navItems = 
+            document.querySelectorAll(".nav-item");
+  
+        let showMenu = false;
+  
+        menuBtn.addEventListener("click", toggleMenu);
+  
+        function toggleMenu() {
+            if (!showMenu) {
+                menuBtn.classList.add("close");
+                menu.classList.add("show");
+                menuNav.classList.add("show");
+                navItems.forEach((item) =>
+                    item.classList.add("show"));
+                navItems.forEach((item) =>
+                    item.classList.remove("close"));
+                showMenu = true;
+            } else {
+                menuBtn.classList.remove("close");
+                menu.classList.remove("show");
+                menuNav.classList.remove("show");
+                navItems.forEach((item) =>
+                    item.classList.add("close"));
+                showMenu = false;
+            }
+        }

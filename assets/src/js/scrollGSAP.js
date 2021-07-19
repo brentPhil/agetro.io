ce0d5e46f360291c6bea58931b426efeea4e8466
brentPHIL <brentagetrophil@gmail.com>
2021-07-19 16:20:14 +0800

    final

--- assets/src/js/scrollGSAP.js
+++ assets/src/js/scrollGSAP.js
@@ -1,10 +1,3 @@
 
-var controller = new ScrollMagic.Controller();
 
-var Scene = new ScrollMagic.Scene({
-    triggerElement:'#about'
-}) 
-    
-.setClassToggle('.illus img', 'fade-in')
-.addTo(controller);
     

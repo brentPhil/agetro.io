ae690ddaa5543bc01577159e510625d000982916
brentPHIL <brentagetrophil@gmail.com>
2021-07-25 20:53:32 +0800

    final

--- assets/src/js/navburger.js
+++ assets/src/js/navburger.js
@@ -49,20 +49,49 @@ function copyToClipboard(element) {
 
 const cursor = document.querySelector('.epiccursor');
 const cursor1 = document.querySelector('.cursormain');
+const visit = document.querySelector('.visitSite')
+const caseStudy = document.querySelector('.case');
+const caseM = document.querySelector('.caseStudy');
+const addcopy = document.querySelector('.addcopy');
 
 document.addEventListener('mousemove', e =>{
  cursor.setAttribute("style", "top:"+(e.clientY - 15)+"px; left: "+(e.clientX - 15)+"px;")
  cursor1.setAttribute("style", "top:"+(e.clientY - 2)+"px; left: "+(e.clientX - 2)+"px;")
+ visit.setAttribute("style", "top:"+(e.clientY - 38)+"px; left: "+(e.clientX - 38)+"px;")
+ caseM.setAttribute("style", "top:"+(e.clientY - 38)+"px; left: "+(e.clientX - 38)+"px;")
+ addcopy.setAttribute("style", "top:"+(e.clientY - 38.5)+"px; left: "+(e.clientX - 38.5)+"px;")
 });
 
 document.addEventListener('click', () =>{
   cursor.classList.add("expand");
-
   setTimeout(() => {
     cursor.classList.remove("expand");
   }, 500);
 } );
-const links = document.querySelectorAll('body a');
+
+site.addEventListener('mouseover', () =>{
+  cursor.classList.add('shrink');
+  visit.classList.add('sitegrow');
+});
+
+site.addEventListener('mouseout', () =>{
+  cursor.classList.remove('shrink');
+  visit.classList.remove('sitegrow');
+});
+
+caseStudy.addEventListener('mouseover', () =>{
+  cursor.classList.add('shrink');
+  caseM.classList.add('sitegrow');
+});
+
+caseStudy.addEventListener('mouseout', () =>{
+  cursor.classList.remove('shrink');
+  caseM.classList.remove('sitegrow');
+});
+
+
+const links = document.querySelectorAll('.sitenav');
+
 links.forEach((link) => {
   link.addEventListener('mouseover', () =>{
     cursor.classList.add("grow");
@@ -73,3 +102,14 @@ links.forEach((link) => {
     cursor.classList.remove("grow");
   });
 });
+
+
+  btn.addEventListener('mouseover', () =>{
+    addcopy.classList.add("sitegrow");
+    cursor.classList.add('shrink');
+  });
+
+  btn.addEventListener('mouseout', () =>{
+    addcopy.classList.remove("sitegrow");
+    cursor.classList.remove('shrink');
+  });
